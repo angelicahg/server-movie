206de0806e864dea77f142cd81da49dc5759a0bd
angelicahg <herrerangelik@gmail.com>
2021-10-20 19:05:42 -0400

    servidor

--- config/conexion.js
+++ config/conexion.js
@@ -6,7 +6,6 @@ const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
 
 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./config/conexion');
+const express = require('express')
+const cors = require('cors')
+const host = '0.0.0.0';
 
-const express = require('express');
 const port = (process.env.PORT || 3000);
 
 // express
@@ -9,6 +11,8 @@ const app = express();
 //admitir
 app.use(express.json())
 
+// cors
+app.use(cors())
 
 //configurar
 app.set('port', port)
@@ -18,7 +22,7 @@ app.use('/api', require('./rutas'))
 
 
 //inicializar express
-app.listen(app.get('port'), (error) => {
+app.listen(app.get(port, host), (error) => {
   if (error) { console.log('error al iniciar el servidor: ' + error) }
   else {
     console.log('servidor iniciado en el puerto: ' + port)
